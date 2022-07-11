a08b7f6794df860295763c22ce7b148e468fc39a
ammannn <memandeep7@gmail.com>
2022-07-11 23:44:51 +0530

    initial screen UI starter for rador

--- App.js
+++ App.js
@@ -1,131 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { GeoFire } from "geofire";
-import firebase from "firebase";
-import { useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React from 'react';
+import { View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/tabs';
 
 export default function App() {
-  const uid = uuidv4();
-  useEffect(() => {
-    const firebaseConfig = {
-      apiKey: "AIzaSyAYL85a8H6c-_9fU4OtDE6LLTkVOJJIqAg",
-      authDomain: "native-base-hack.firebaseapp.com",
-      databaseURL: "https://native-base-hack-default-rtdb.firebaseio.com",
-      projectId: "native-base-hack",
-      storageBucket: "native-base-hack.appspot.com",
-      messagingSenderId: "600550657392",
-      appId: "1:600550657392:web:2431af57f1ac899cd613b5",
-    };
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    }
-
-    var ref = firebase.database().ref();
-    const geofireRef = new GeoFire(firebase.database().ref("getData"));
-    let lat = 12.8832182;
-    let long = 77.6048333;
-    let users = [
-      [40.73061, -73.935242],
-      [2.8832182, 77.6048333],
-    ];
-
-    geofireRef.set(uid, users[0]);
-    var geoQuery = geofireRef.query({
-      center: users[0],
-      radius: 10.5,
-    });
-
-    var onReadyRegistration = geoQuery.on("ready", function () {
-      console.log(
-        "GeoQuery has loaded and fired all other events for initial data"
-      );
-    });
-
-    var onKeyEnteredRegistration = geoQuery.on(
-      "key_entered",
-      function (key, location, distance) {
-        console.log(
-          key +
-            " entered query at " +
-            location +
-            " (" +
-            distance +
-            " km from center)"
-        );
-      }
-    );
-
-    var onKeyExitedRegistration = geoQuery.on(
-      "key_exited",
-      function (key, location, distance) {
-        console.log(
-          key +
-            " exited query to " +
-            location +
-            " (" +
-            distance +
-            " km from center)"
-        );
-      }
-    );
-
-    return () => {
-      geoFire.remove(uid).then(
-        function () {
-          console.log("Provided key has been removed from GeoFire");
-        },
-        function (error) {
-          console.log("Error: " + error);
-        }
-      );
-
-      var onKeyEnteredRegistration = geoQuery.on(
-        "key_entered",
-        function (key, location, distance) {
-          console.log(
-            key +
-              " entered query at " +
-              location +
-              " (" +
-              distance +
-              " km from center)"
-          );
-        }
-      );
-
-      var onKeyExitedRegistration = geoQuery.on(
-        "key_exited",
-        function (key, location, distance) {
-          console.log(
-            key +
-              " exited query to " +
-              location +
-              " (" +
-              distance +
-              " km from center)"
-          );
-
-          // Cancel all of the query's callbacks
-          geoQuery.cancel();
-        }
-      );
-    };
-  }, []);
-
   return (
-    <View style={styles.container}>
-      <Text>I am from BLR</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ navigation/tabs.js
@@ -0,0 +1,217 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
+import { useRef, useEffect } from 'react';
+
+//screens
+import Home from '../screens/Home';
+import Setting from '../screens/Setting';
+
+//icons
+import {
+  MaterialCommunityIcons,
+  MaterialIcons,
+  FontAwesome,
+  Ionicons,
+} from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+//custom tabbar button
+
+const CustomTabBarButton = ({ children, onPress }) => (
+  <View
+    onPress={onPress}
+    style={{
+      top: -30,
+      justifyContent: 'center',
+      alignItems: 'center',
+      transform: [{ rotate: '45deg' }],
+    }}
+  >
+    <View
+      style={{
+        borderRadius: 23,
+        backgroundColor: '#101010',
+        padding: 5,
+        zIndex: -2,
+        // ...styles.shadow,
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          width: 70,
+          height: 70,
+          borderRadius: 23,
+          backgroundColor: 'rgb(81, 189, 255);',
+        }}
+      >
+        {children}
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+const Tabs = () => {
+  const anim = useRef(new Animated.Value(1));
+
+  useEffect(() => {
+    // makes the sequence loop
+    Animated.loop(
+      // runs given animations in a sequence
+      Animated.sequence([
+        // increase size
+        Animated.timing(anim.current, {
+          toValue: 1.2,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+        // decrease size
+        Animated.timing(anim.current, {
+          toValue: 1,
+          duration: 700,
+          useNativeDriver: true,
+        }),
+      ]),
+    ).start();
+  }, []);
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarComponent: {},
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          borderTopWidth: 0,
+          position: 'absolute',
+          bottom: 25,
+          left: 20,
+          right: 20,
+          elevation: 0,
+          backgroundColor: 'rgba(255,255,255, 0.1)',
+          borderRadius: 25,
+          height: 80,
+          ...styles.shadow,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                top: 12,
+              }}
+            >
+              <MaterialCommunityIcons
+                name="home-flood"
+                size={30}
+                color={focused ? '#ffff' : '#696969'}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                top: 12,
+              }}
+            >
+              <Ionicons
+                name="chatbubble-ellipses"
+                size={30}
+                color={focused ? '#ffff' : '#696969'}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Check"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Animated.View style={{ transform: [{ scale: anim.current }] }}>
+              <Ionicons name="ios-locate-outline" size={30} color="#ffff" />
+            </Animated.View>
+          ),
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Check1"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                top: 12,
+              }}
+            >
+              <MaterialIcons
+                name="settings"
+                size={30}
+                color={focused ? '#ffff' : '#696969'}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Check2"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                top: 12,
+              }}
+            >
+              <FontAwesome
+                name="user"
+                size={30}
+                color={focused ? '#ffff' : '#696969'}
+              />
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#ffff',
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+});
+
+export default Tabs;
