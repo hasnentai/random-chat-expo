1b649215384ba6160d765e1822ca00db6e1b799e
ammannn <memandeep7@gmail.com>
2022-07-13 21:52:19 +0530

    update for native base

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import Compass from "./components/compass";
+import Compass from './components/compass';
 
-import ChatScreen from "./screens/chat-screen";
-import NearByScreen from "./screens/near-by-screen";
-import { setUpFireBase } from "./utils/geoFire";
-import LoginScreen from "./screens/login-screen";
-import RootNavigation from "./navigation";
+import ChatScreen from './screens/chat-screen';
+import NearByScreen from './screens/near-by-screen';
+import { setUpFireBase } from './utils/geoFire';
+import LoginScreen from './screens/login-screen';
+import RootNavigation from './navigation';
+import { NativeBaseProvider } from 'native-base';
+import { nativeBaseHackTheme } from './theme/NativeBaseHackTheme';
 
 export default function App() {
-  return <RootNavigation />;
+  return (
+    <NativeBaseProvider theme={nativeBaseHackTheme}>
+      <RootNavigation />
+    </NativeBaseProvider>
+  );
 }
