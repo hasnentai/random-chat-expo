22e63f7acb7ec9989df2fd26887aca9d2a8af4ff
ammannn <memandeep7@gmail.com>
2022-07-17 10:08:19 +0530

    bug for phone auth

--- navigation/index.js
+++ navigation/index.js
@@ -1,15 +1,15 @@
-import { getItemAsync } from "expo-secure-store";
-import React, { useEffect, useState } from "react";
-import { getValueFor } from "../utils/secure-store";
-import AuthStack from "./auth-stack";
-import UserStack from "./user-stack";
+import { getItemAsync } from 'expo-secure-store';
+import React, { useEffect, useState } from 'react';
+import { getValueFor } from '../utils/secure-store';
+import AuthStack from './auth-stack';
+import UserStack from './user-stack';
 
 export default function RootNavigation() {
   const [verificationId, setVerificationId] = useState(null);
 
   useEffect(() => {
     async function getUserVerificationId() {
-      let key = await getValueFor("verificationId");
+      let key = await getValueFor('access-token');
       setVerificationId(key);
     }
     getUserVerificationId();
